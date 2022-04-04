57f8b6fc71e86042476b8195419810ba61f6e2a2
ylan0007 <ylan0007@student.monash.edu>
2022-04-04 13:09:26 +0800

    call function version 0.1

--- app/src/main/java/com/example/ptsafe/EmergencyFragment.java
+++ app/src/main/java/com/example/ptsafe/EmergencyFragment.java
@@ -1,5 +1,7 @@
 package com.example.ptsafe;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,10 +9,16 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.ptsafe.R;
 
 public class EmergencyFragment extends Fragment {
+
+    private Button call;
+
+
+
 
     public EmergencyFragment() {
 
@@ -20,6 +28,15 @@ public class EmergencyFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the View for this fragment
         View view = inflater.inflate(R.layout.fragment_emergency, container, false);
+        call = view.findViewById(R.id.call);
+        call.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_CALL);
+                Uri data = Uri.parse("tel:" + "111");
+                intent.setData(data);
+                startActivity(intent);
+            }});
         return view;
 
     }
