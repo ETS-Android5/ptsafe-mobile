8405494126f2097c71fb4348ca9fb347376b7d6f
ylan0007 <ylan0007@student.monash.edu>
2022-04-04 15:37:03 +0800

    call function version 0.1

--- app/src/main/java/com/example/ptsafe/EmergencyFragment.java
+++ app/src/main/java/com/example/ptsafe/EmergencyFragment.java
@@ -1,11 +1,16 @@
 package com.example.ptsafe;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,6 +21,7 @@ import com.example.ptsafe.R;
 public class EmergencyFragment extends Fragment {
 
     private Button call;
+    private static final String TAG = "MoreInfoActivity";
 
 
 
@@ -32,10 +38,16 @@ public class EmergencyFragment extends Fragment {
         call.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Intent.ACTION_CALL);
-                Uri data = Uri.parse("tel:" + "111");
-                intent.setData(data);
-                startActivity(intent);
+                if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    requestPermissions(new String[]{Manifest.permission.CALL_PHONE},10010);
+                    Log.d(TAG, "request_permission():Requesting permission");
+                }else {
+                    Log.d(TAG, "request_permission():Already have permission!");
+                    Intent intent = new Intent(Intent.ACTION_CALL);
+                    Uri data = Uri.parse("tel:" + "111");
+                    intent.setData(data);
+                    startActivity(intent);
+                }
             }});
         return view;
 
