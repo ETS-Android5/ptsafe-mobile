e32fb1d13122f17e874aa6d175e0c8f8002c8c43
Yonas Mulyadi <ymul0001@student.monash.edu>
2022-05-04 21:54:54 +1000

    modify some titles before showcase

--- app/src/main/java/com/example/ptsafe/FindStationFragment.java
+++ app/src/main/java/com/example/ptsafe/FindStationFragment.java
@@ -56,7 +56,7 @@ public class FindStationFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_find_station, container, false);
-        getActivity().setTitle("Your trips");
+        getActivity().setTitle("My trips");
         initVars();
         initView(view);
         getAllTripWishlistData();
